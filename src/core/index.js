0377f653760e074c9caf5ed9b801633c79e3b3a2
Dastan Abdraym <dastan.abdraym@mail.ru>
2022-02-09 19:16:36 +0600

    убрал метод для валидации токена

--- src/core/index.js
+++ src/core/index.js
@@ -7,4 +7,3 @@ export { default as getAccessToken } from './get-access-token';
 export { default as getRefreshToken } from './get-refresh-token';
 export { default as logout } from './logout';
 export { default as parseToken } from './parse-token';
-export { default as validateToken } from './validate-token';
